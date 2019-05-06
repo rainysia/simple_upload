df1b1ae4c1e77a299f011cf84ac0b255a248a2ca
rainysia <rainysia@gmail.com>
2019-05-06 17:13:47 +0800

    update supervisord_watching for specified supervisor job

--- supervisord_watching.sh
+++ supervisord_watching.sh
@@ -12,15 +12,16 @@
 # * @author     Rainy Sia <rainysia@gmail.com>
 # * @copyright  2013-2016 BTROOT.ORG
 # * @license    https://opensource.org/licenses/MIT license
-# * @version    GIT: 0.0.1
+# * @version    GIT: 0.0.2
 # * @createTime 2018-06-19 10:41:06
-# * @lastChange 2018-06-19 10:41:06
+# * @lastChange 2019-05-06 17:13:25
 
 # * @link http://www.btroot.org
 #*
 set -e
 log_path='/var/log/supervisor/'
 now=`date '+%F %H:%M:%S'`
+spname='dddd'
 if [ ! -d "$log_path" ]; then
     mkdir "$log_path"
 fi
@@ -28,5 +29,13 @@ supervisord_res=`ps aux | grep '/usr/bin/supervisord' | grep -v 'grep' | awk ' {
 if [ ! -n "$supervisord_res" ]; then
     /bin/bash /etc/init.d/supervisor start
     echo "\n=============start=================\n$now restart supervisor by Tom because it's stop\n=============end===================" >> $log_path"supervisord.log"
+else
+    supervisord_process=`supervisorctl status | grep $spname | awk ' { print NR=$2 }' | sed -n 1p`
+    if [ -n "$supervisord_process" ]; then
+        if [ $supervisord_process != 'RUNNING' ]; then
+            supervisord_process_name=`supervisorctl status | grep $spname | sed -n 1p | awk ' { print NR=$1 }'`
+            supervisorctl restart $supervisord_process_name
+            echo "\n=============start=================\n$now restart $supervisord_process_name by Tom because it's Stopped \n=============end===================" >> $log_path"supervisord.log"
+        fi
+    fi
 fi
-
